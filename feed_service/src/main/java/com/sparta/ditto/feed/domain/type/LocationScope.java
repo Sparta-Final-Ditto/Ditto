1e1e9b0a46da69a8368420f312a42e2e23d5af60
euneuneun <daeun2597@gmail.com>
2026-06-18 15:51:55 +0900

    feat: feed-service domain 계층 구현

---
+++ feed_service/src/main/java/com/sparta/ditto/feed/domain/type/LocationScope.java
@@ -0,0 +1,5 @@
+package com.sparta.ditto.feed.domain.type;
+
+public enum LocationScope {
+    PUBLIC, FOLLOWERS_ONLY, PRIVATE
+}
---
+++ feed_service/src/main/java/com/sparta/ditto/feed/domain/type/OutboxStatus.java
@@ -0,0 +1,5 @@
+package com.sparta.ditto.feed.domain.type;
+
+public enum OutboxStatus {
+    PENDING, PUBLISHED, FAILED
+}
