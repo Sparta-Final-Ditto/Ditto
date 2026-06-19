2c5c2737eacc3294aa960e7b93f2a6706b9dded4
Seonjin-13 <sunjin1101@pusan.ac.kr>
2026-06-19 12:16:25 +0900

    feat: EmbeddingErrorCode에 PROFILE_NOT_FOUND(EMBED-004) 추가

--- embedding_service/app/common/exception/error_code.py
+++ embedding_service/app/common/exception/error_code.py
@@ -27,3 +27,4 @@ class EmbeddingErrorCode(ErrorCode):
     EMBEDDING_NOT_FOUND = ("EMBED-001", "해당 게시글의 임베딩 정보를 찾을 수 없습니다.", 404)
     EMBEDDING_FAILED    = ("EMBED-002", "임베딩 처리 중 오류가 발생했습니다.", 500)
     ALREADY_EMBEDDED    = ("EMBED-003", "이미 처리된 임베딩입니다.", 409)
+    PROFILE_NOT_FOUND   = ("EMBED-004", "해당 유저의 프로필 임베딩 정보를 찾을 수 없습니다.", 404)
