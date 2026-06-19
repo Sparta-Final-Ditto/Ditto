7fedb55c70cdc3aab8785acc6f3b10af7038e67e
GolemOnce <shh3350@gmail.com>
2026-06-19 14:53:32 +0900

    fix: checkstyle 반영

--- api-gateway/src/main/java/com/sparta/ditto/gateway/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/sparta/ditto/gateway/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.sparta.ditto.gateway.filter;
 
 import com.sparta.ditto.gateway.util.JwtUtil;
 import io.jsonwebtoken.Claims;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -11,8 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
--- api-gateway/src/main/java/com/sparta/ditto/gateway/util/JwtUtil.java
+++ api-gateway/src/main/java/com/sparta/ditto/gateway/util/JwtUtil.java
@@ -3,12 +3,11 @@ package com.sparta.ditto.gateway.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
