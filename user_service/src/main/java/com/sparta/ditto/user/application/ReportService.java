7ee62955a443e3f412bef4f26c9455f493f85908
GolemOnce <shh3350@gmail.com>
2026-06-23 12:30:32 +0900

    feat: 신고 API 구현

---
+++ user_service/src/main/java/com/sparta/ditto/user/application/ReportService.java
@@ -0,0 +1,37 @@
+package com.sparta.ditto.user.application;
+
+import com.sparta.ditto.user.domain.report.Report;
+import com.sparta.ditto.user.domain.report.exception.AlreadyReportedException;
+import com.sparta.ditto.user.domain.report.exception.CannotSelfReportException;
+import com.sparta.ditto.user.domain.user.User;
+import com.sparta.ditto.user.domain.user.exception.UserNotFoundException;
+import com.sparta.ditto.user.infrastructure.repository.ReportRepository;
+import com.sparta.ditto.user.infrastructure.repository.UserRepository;
+import com.sparta.ditto.user.presentation.dto.request.ReportRequest;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+
+    private final ReportRepository reportRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void report(UUID reporterId, UUID reportedId, ReportRequest request) {
+        if (reporterId.equals(reportedId)) {
+            throw new CannotSelfReportException();
+        }
+        if (reportRepository.existsByReporterIdAndReportedId(reporterId, reportedId)) {
+            throw new AlreadyReportedException();
+        }
+
+        User reporter = userRepository.findById(reporterId).orElseThrow(UserNotFoundException::new);
+        User reported = userRepository.findById(reportedId).orElseThrow(UserNotFoundException::new);
+
+        reportRepository.save(Report.of(reporter, reported, request.reportType(), request.content()));
+    }
+}
