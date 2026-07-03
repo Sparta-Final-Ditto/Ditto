1a4ebe2ffdf3db8cd9906b91cf033293d3b85867
musoyou12 <charlie123000@gmail.com>
2026-07-03 14:18:36 +0900

    [Refactor] MatchService유닛 테스트

---
+++ match_service/src/test/java/com/sparta/ditto/match/infrastructure/Llm/OllamaAdapterTest.java
@@ -0,0 +1,4 @@
+package com.sparta.ditto.match.infrastructure.Llm;
+
+public class OllamaAdapterTest {
+}
---
+++ match_service/src/test/java/com/sparta/ditto/match/infrastructure/metrics/MatchMetricsServiceTest.java
@@ -0,0 +1,4 @@
+package com.sparta.ditto.match.infrastructure.metrics;
+
+public class Matchmetricsservicetest {
+}
