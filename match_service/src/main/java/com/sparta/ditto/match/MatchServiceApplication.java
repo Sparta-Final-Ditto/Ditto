a48f5f419b7eaed44f444311f739cac6bc9b18d5
musoyou12 <charlie123000@gmail.com>
2026-07-03 11:50:26 +0900

    feat: 하이브리드 후보자 검색 및 pgvector 기반 벡터 검색 기능 구현
    - EmbeddingSyncService 및 VectorSearchService 추가
    - HNSW 인덱스를 적용한 V2 마이그레이션 스크립트 작성
    - 검색 성능 및 생성 신뢰도 테스트 코드 수정

--- match_service/src/main/java/com/sparta/ditto/match/MatchServiceApplication.java
+++ match_service/src/main/java/com/sparta/ditto/match/MatchServiceApplication.java
@@ -3,9 +3,12 @@ package com.sparta.ditto.match;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class MatchServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MatchServiceApplication.class, args);
