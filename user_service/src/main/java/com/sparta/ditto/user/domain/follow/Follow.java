07f1afbc554ff7914e5cee1b96450724247c8bbc
GolemOnce <shh3350@gmail.com>
2026-07-09 01:53:58 +0900

    refactor: UUID id 필드 제거(BaseEntity extends 엔티티)

--- user_service/src/main/java/com/sparta/ditto/user/domain/follow/Follow.java
+++ user_service/src/main/java/com/sparta/ditto/user/domain/follow/Follow.java
@@ -4,19 +4,15 @@ import com.sparta.ditto.common.entity.BaseEntity;
 import com.sparta.ditto.user.domain.user.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import java.util.Objects;
-import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.UuidGenerator;
 
 @Getter
 @Entity
@@ -33,11 +29,6 @@ import org.hibernate.annotations.UuidGenerator;
 )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Follow extends BaseEntity {
-
-    @Id
-    @GeneratedValue
-    @UuidGenerator
-    private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "follower_id", nullable = false)
